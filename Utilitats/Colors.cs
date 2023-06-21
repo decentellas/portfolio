bddac861704acf8c327859e5925776e1def8f07b
David <descarre.cifo@gmail.com>
2023-06-21 22:50:04 +0200

    Build(core) Catventari 1.0 app desktop

--- Utilitats/Colors.cs
+++ Utilitats/Colors.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+
 
 namespace APP_Catventari
 {
     /*
-     * Definde Colors
+     * Define Colors
      * 
     */
     public class Colors
